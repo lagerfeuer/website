6b827be60b77faf48a5c2fbdd6abd04ff9006e73
Lukas Deutz <git@deutz.dev>
2026-08-11 09:11:36 -0400

    Remove Plausible

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,21 +1,18 @@
 import "../styles/globals.css";
 import Layout from "../components/layout";
 import Head from "next/head";
-import PlausibleProvider from "next-plausible";
 import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <PlausibleProvider domain="deutz.io">
-        <Head>
-          <title>Lukas&apos; Website</title>
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-          <Analytics />
-        </Layout>
-      </PlausibleProvider>
+      <Head>
+        <title>Lukas&apos; Website</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
     </>
   );
 }
